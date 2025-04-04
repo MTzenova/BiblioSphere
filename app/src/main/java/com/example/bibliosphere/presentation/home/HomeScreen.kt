2c92ea9e8a39f2e1a45c2987fc636556ed15e5d7
MonicaTz97 <monica_97_ct@hotmail.com>
2025-04-04 20:32:31 +0200

    Pantalla HomeScreen creada

---
+++ app/src/main/java/com/example/bibliosphere/presentation/home/HomeScreen.kt
@@ -0,0 +1,60 @@
+package com.example.bibliosphere.presentation.home
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bibliosphere.R
+import com.example.bibliosphere.presentation.components.AutoResizedText
+import com.example.bibliosphere.presentation.theme.BiblioSphereTheme
+
+@Composable
+fun HomeScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(BiblioSphereTheme.dimens.paddingMedium)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.TopEnd
+        ){
+            Icon(//esto lo podemos usar para el menú lateral
+                modifier = Modifier.size(BiblioSphereTheme.dimens.iconSizeNormal),
+                painter = painterResource(id = R.drawable.logo_bibliosphere), //cambiar esto por icono de menú
+                contentDescription = "Menu",
+                tint = MaterialTheme.colorScheme.primary,
+            )
+        }
+        AutoResizedText(
+            text = "BiblioSphere",
+            textStyle = MaterialTheme.typography.displayMedium.copy(
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center
+            ),
+        )
+        Spacer(modifier = Modifier.height(BiblioSphereTheme.dimens.spacerMedium)) //espacio entre elementos
+
+    }
+}
+
+@Preview(
+    name = "HomeScreenPreview",
+    showBackground = true,
+)
+@Composable
+fun HomeScreenPreview(){
+    BiblioSphereTheme {
+        HomeScreen()
+    }
+}
